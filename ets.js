fe441fe9a8dbb3e95997de1e31d40e05910907a8
calyeefornia <e0175177@u.nus.edu>
2018-01-16 19:40:24 +0800

    added set grid function

--- ets.js
+++ ets.js
@@ -48,8 +48,29 @@ let setRainbow = () =>{
 let rainbowBtn = document.querySelector("#rainbow");
 rainbowBtn.addEventListener("click", setRainbow);
 
+let setGrid = () => {
+	let length = window.prompt("Enter a value from 1 to 100");
+	let children = document.querySelectorAll(".row");
+	children.forEach((child) => container.removeChild(child));
 
+	for(let r = 0; r < length; r++){
+	let row = document.createElement("div");
+	row.setAttribute("class", "row");
+	for(let c = 0; c < length; c++){
+		let col = document.createElement("div");
+		col.setAttribute("style", "width: " + 640/length + "px; height: " + 640/length+ "px;");
+		col.setAttribute("class", "square");
+		row.appendChild(col);
+	}
+	container.appendChild(row);
+	}
+	let squares = document.querySelectorAll(".square");
+	squares.forEach(square => square.addEventListener("mouseenter", drawColor));
 
+}
+
+let setGridBtn = document.querySelector("#setgrid");
+setGridBtn.addEventListener("click", setGrid);
 
 
 
